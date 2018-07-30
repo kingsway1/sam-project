a14f02202e7c4fb6bf529cb5461ffefca4a146a0
root <sambohnel1@gmail.com>
2018-07-30 15:10:30 +0100

    mount.sh update

--- mount.sh
+++ mount.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#Sam Bohnel
+#30/07/18
+
+#README
+
+#The system the script is running on must have at least two hard disks, the second hard disk must be bigger in size than the first hard disk.   
 
 echo -e "\e[00;32m#######################################################################\e[00m"
 echo ""
@@ -23,6 +29,7 @@ echo -e "\e[00;32m##############################################################
 
 #mount disk
 
+
 printf "Choose the disk you want to mount - for example /dev/sdb1 \n"
 
 read disk 
@@ -35,26 +42,32 @@ mkdir /mnt/$location
 
 mount $disk /mnt/$location
 
+mount | grep /mnt/$location
+
 #copy /dev/sda to mount point
 
 printf "copying /dev/sda to disk.img file in mount point"
 
+# hard coded at the moment - need to find a method for working out bs and count 
 if mountpoint -q /mnt/$location
 then 
-   dd if=/dev/sda of=/mnt/$location/disk.img bs=x count=x
+   dd if=/dev/sda of=/mnt/$location/disk.img bs=1073741824 count=6
 else
    echo "not mounted"
+   #repeat stage - ensure its mounted
 fi
 
 #verify if disk.img was succesfully created
 
 #copy disk.img to qemu host 
 
-printf "copying disk to qemu host - select scp destination\n"
+printf "copying disk to qemu host - select scp destination - for example root@192.168.33.159:/root\n"
 printf "scp disk.img " 
 read destination
 
 scp /mnt/$location/disk.img $destination
+
+
 
 #if success then finish 
 #else enter valid location
