9a78e32e0cbbc8865000c3bdfe8e24dccd0a836a
kingsway1 <sambohnel1@gmail.com>
2018-08-11 18:04:52 +0100

    update

--- qemu-boot-script.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-x86_64 -enable-kvm disk.img -m 512 -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device e1000,netdev=net0,mac=00:aa:00:60:00:01
--- test.sh
+++
@@ -1,3 +0,0 @@
-printf "copying disk to qemu host - select location\n"
-printf "scp disk.img " 
-read location
