ec92f525519def6683e6eb5425969d41d6e1065e
kingsway1 <sambohnel1@gmail.com>
2018-08-30 14:02:51 +0100

    final commit

--- SystemCloningSolution/clone-deploy/clone-deployment.py
+++ SystemCloningSolution/clone-deploy/clone-deployment.py
@@ -20,17 +20,17 @@ location=options.location
 
 # call qemu-ifup script in /etc
 
-#os.system('/etc/qemu-ifup')
+os.system('/etc/qemu-ifup')
 
 #create directories, copy files and set up tap devices -- will increment with each device
 x = 0
 y = 1
 while x < int(number):
-    os.system('echo mkdir %s/copydir_%d' %(location, y))
-    os.system('echo cp %s %s/copydir_%d/copy%d_of_%s' %(filename, location, y, y, filename))
-    os.system('echo ip tuntap add dev tap%d mode tap user $(whoami)' % y)
-    os.system('echo ip link set tap%d master br0' % y)
-    os.system('echo ip link set dev tap%d up' % y)
+    os.system('mkdir %s/copydir_%d' %(location, y))
+    os.system('cp %s %s/copydir_%d/copy%d_of_%s' %(filename, location, y, y, filename))
+    os.system('ip tuntap add dev tap%d mode tap user $(whoami)' % y)
+    os.system('ip link set tap%d master br0' % y)
+    os.system('ip link set dev tap%d up' % y)
     x+=1
     y+=1
     time.sleep(1)
