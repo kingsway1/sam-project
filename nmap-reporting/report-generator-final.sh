a1a1e34f8d29814229ed7634c2752bc9ecb7d0bd
kingsway1 <sambohnel1@gmail.com>
2018-08-30 13:33:54 +0100

    tidy up

--- nmap-reporting/report-generator-final.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-printf "Set Target: " 
-read IP
-
-nmap -sV $IP -oX scan.xml && xsltproc scan.xml -o "/root/samProject/nmapReports/$IP-`date +%d:%m:%y`_report.html" 
-
-echo ""
-printf "$IP-`date +%d:%m:%y`_report.html saved in /root/samProject/nmapReports"
-echo ""
